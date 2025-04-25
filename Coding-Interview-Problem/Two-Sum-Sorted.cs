d5cfa3e003e97327f0940cb9419fe1e32c5152b7
sudharsan <susuma1998@gmail.com>
2025-04-25 20:12:28 +0530

    Two sum sorted problem

---
+++ Coding-Interview-Problem/Two-Sum-Sorted.cs
@@ -0,0 +1,25 @@
+﻿//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+
+int[] TwoSum(int[] numbers, int target)
+{
+    int left = 0;
+    int right = numbers.Length - 1;
+    while (left < right)
+    {
+        var sum = numbers[left] + numbers[right];
+        if (target == sum)
+        {
+            return [left + 1, right + 1];
+        }
+        else if (sum > target)
+        {
+            right -= 1;
+        }
+        else if (sum < target)
+        {
+            left += 1;
+        }
+    }
+    return [];
+}
