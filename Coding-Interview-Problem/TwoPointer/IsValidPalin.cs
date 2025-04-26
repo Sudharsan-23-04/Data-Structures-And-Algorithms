8249a14ac0eef201609174d51b1c17984dab72a6
sudharsan <susuma1998@gmail.com>
2025-04-26 18:51:57 +0530

    Valid palindrome and notes pdf

---
+++ Coding-Interview-Problem/TwoPointer/IsValidPalin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Interview_Problem.TwoPointer
+{
+    public static partial class TwoPointer
+    {
+        public static bool IsValidPalin(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            var left = 0;
+            var right = str.Length - 1;
+
+            while(left < right)
+            {
+                if (str[left] != str[right])
+                {
+                    return false;
+                }
+                else
+                {
+                    left += 1;
+                    right -= 1;
+                }
+            }
+
+            return true;
+        }
+    }
+}
