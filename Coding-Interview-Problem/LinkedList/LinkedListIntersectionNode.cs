db70a56ea2d3fae0793a4c78fc9eed074c56c5c4
Sudharsan <sudharsanbalasubramanian1@gmail.com>
2025-05-03 17:43:55 +0530

    Intersection of linked list

---
+++ Coding-Interview-Problem/LinkedList/LinkedListIntersectionNode.cs
@@ -0,0 +1,19 @@
+﻿namespace Coding_Interview_Problem.LinkedList
+{
+    public static partial class LinkedList
+    {
+        public static LinkedList<T> GetIntersectionNode<T>(LinkedList<T> headA, LinkedList<T> headB)
+        {
+            var pointerA = headA;
+            var pointerB = headB;
+
+            while (pointerA != pointerB)
+            {
+                pointerA = pointerA is null ? headB : pointerA.Next;
+                pointerB = pointerB is null ? headA : pointerB.Next;
+            }
+
+            return pointerA;
+        }
+    }
+}
--- Coding-Interview-Problem/LinkedList/RemoveNthFromEnd.cs
+++ Coding-Interview-Problem/LinkedList/RemoveNthFromEnd.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Coding_Interview_Problem.LinkedList
+﻿namespace Coding_Interview_Problem.LinkedList
 {
     public static partial class LinkedList
     {
