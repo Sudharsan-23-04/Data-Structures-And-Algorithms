c4eab9b6363ffd0d41db8ca2366628e59ee74dbb
Sudharsan <sudharsanbalasubramanian1@gmail.com>
2025-05-04 20:50:56 +0530

    cycle detection

---
+++ Coding-Interview-Problem/FastAndSlowPointer/CycleDetection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Interview_Problem.FastAndSlowPointer
+{
+    public static partial class FastAndSlowPointer
+    {
+        public static bool HasCycle<T>(LinkedList<T> head)
+        {
+            var slow = head;
+            var fast = head;
+
+            while (slow is not null && fast is not null && fast.Next is not null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
