45f2cecfa277da0e59dc9a9b5804872b447bb122
Sudharsan <sudharsanbalasubramanian1@gmail.com>
2025-05-06 09:17:45 +0530

    Happy Number and linked list mid point

---
+++ Coding-Interview-Problem/FastAndSlowPointer/HappyNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Interview_Problem.FastAndSlowPointer
+{
+    public static partial class FastAndSlowPointer
+    {
+        public static bool IsHappy(int n)
+        {
+            var slow = n;
+            var fast = n;
+            while (true)
+            {
+                slow = GetNum(slow);
+                fast = GetNum(GetNum(fast));
+
+                if (fast == 1)
+                {
+                    return true;
+                }
+
+                if (fast == slow)
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        private static int GetNum(int n)
+        {
+            var nextNum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+
+                n = n / 10;
+
+                nextNum += digit * digit;
+            }
+
+            return nextNum;
+        }
+    }
+}
---
+++ Coding-Interview-Problem/FastAndSlowPointer/LinkedListMidPoint.cs
@@ -0,0 +1,19 @@
+﻿namespace Coding_Interview_Problem.FastAndSlowPointer
+{
+    public static partial class FastAndSlowPointer
+    {
+        public LinkedList<T> MiddleNode<T>(LinkedList<T> head)
+        {
+            var slow = head;
+            var fast = head;
+
+            while (slow is not null && fast is not null && fast.Next is not null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            return slow;
+        }
+    }
+}
